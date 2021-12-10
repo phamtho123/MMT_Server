ff44db7f51c798f856ba239dabea52fc51ea741d
phamtho123 <thopham.21082001@gmail.com>
2021-12-10 17:51:17 +0700

    server

---
+++ controllers/posts.js
@@ -0,0 +1,54 @@
+import { PostModel } from '../models/PostModels.js';
+
+export const getPosts = async(req,res)=>{
+    try {
+         const posts = await PostModel.find();
+         console.log('posts',posts);
+         res.status(200).json(posts);
+    } catch (err) {
+         res.status(500).json({error:err});
+    }
+};
+
+export const createPost = async(req,res)=>{
+     
+     try {
+          const newPost = req.body;
+          const post = new PostModel(newPost);
+          await post.save();
+          res.status(200).json(post);
+          
+     } catch (err) {
+          res.status(500).json({error:err});
+     }
+};
+
+export const updatePost = async(req,res)=>{
+     
+     try {
+          const updatePost = req.body;
+          const post = await PostModel.findOneAndUpdate(
+               {  _id:updatePost._id },
+                  updatePost, 
+               {  new : true });
+          res.status(200).json(post);
+          
+     } catch (err) {
+          res.status(500).json({error:err});
+     }
+};
+
+export const deletePost = async(req,res)=>{
+     
+     try {
+          const deletePost = req.body;
+          const post = await PostModel.findByIdAndDelete(
+               {  _id:deletePost._id },
+              
+               );
+          res.status(200).json(post);
+          
+     } catch (err) {
+          res.status(500).json({error:err});
+     }
+};
